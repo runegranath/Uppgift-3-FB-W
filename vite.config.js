b0dc6e027c4e0d29773d37e8d17b5a5dd412ee2b
runegranath <rune.granath@gmail.com>
2026-02-23 07:08:18 +0100

    rollupoptions tillagda

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-export default {
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
   base: "/Uppgift-3-FB-W/",
   css: {
     devSourcemap: true,
   },
-};
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        sass: resolve(__dirname, "src/SASS.html"),
+      },
+    },
+  },
+});
